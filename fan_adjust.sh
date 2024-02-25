0a570cb8c322a33cb38c1f04edb36abc8469b2f8
mech <mech@dmux.xyz>
2024-02-25 10:05:36 -0500

    Update fan_adjust.sh

--- fan_adjust.sh
+++ fan_adjust.sh
@@ -2,13 +2,14 @@
 
 # Converts decimal to hex
 dec_hex=$(printf '%x\n' $1 2>/dev/null)
-username="<ENTER YOUR USERNAME HERE BETWEEN THE QUOTES>"
-pw="<ENTER YOUR PASSWORD HERE BETWEEN THE QUOTES>"
+username="<ENTER YOUR IDRAC USERNAME HERE BETWEEN THE QUOTES>"
+pw="<ENTER YOUR IDRAC PASSWORD HERE BETWEEN THE QUOTES>"
+idip="<ENTER YOUR IDRAC IP BETWEEN THE QUOTES>"
 
 if [[ $1 -lt 100 ]] && [[ $1 -gt 20 ]]; then
 
 	# Sets the fan speed manually to requested value
-	ipmitool -I lanplus -H 192.168.102.225 -U $username -P $pw raw 0x30 0x30 0x02 0xff 0x$dec_hex
+	ipmitool -I lanplus -H $idip -U $username -P $pw raw 0x30 0x30 0x02 0xff 0x$dec_hex
 
 	# Lets the user know what they have done
 	echo "$USER has set the fan speed to $1%."
